5949d21cf2c1e4a3641bd5072ed7bff393b714d7
Elisey <enovikow6@gmail.com>
2018-03-28 23:57:07 +0300

    Add Java comments example for lesson 2.4

--- 1_FirstProgram_Comments/src/FirstProgram.java
+++ 1_FirstProgram_Comments/src/FirstProgram.java
@@ -1,5 +1,19 @@
+/**
+ * Класс FirstProgram наш первый класс на языке Java
+ * @version Это первая обучающая программа
+ * @author Elisey Novikov
+ */
+
 public class FirstProgram {
+
+    /*
+     Данный метод является точкой входа в нашу программу.
+     Параметр args является массивом строк,
+     принимающим параметры через пробел при запуске программы
+     */
     public static void main(String[] args) {
+
+        // Данный метод выводит сообщение на консоль
         System.out.println("My first program on Java");
     }
 }
