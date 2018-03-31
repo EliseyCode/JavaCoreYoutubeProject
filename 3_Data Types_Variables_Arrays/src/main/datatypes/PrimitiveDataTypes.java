f36a301f2c23045b2410b5cd8195d3427b4031a7
Elisey <enovikow6@gmail.com>
2018-04-01 00:44:11 +0300

    Java primitives data types example for lesson 3.1

--- 3_Data Types_Variables_Arrays/src/main/datatypes/PrimitiveDataTypes.java
+++ 3_Data Types_Variables_Arrays/src/main/datatypes/PrimitiveDataTypes.java
@@ -6,7 +6,8 @@ public class PrimitiveDataTypes {
     byte b = 27;                // от -128 до 127
     short s = 12535;            // от -32768 до 32767
     int i = 128540;             // от -2147483648 до 2147483647
-    long l = 5_000_000_000l;    // от -9223372036854775808 до 9223372036854775807
+    long l = 5_000_000_000L;    // от -9223372036854775808 до 9223372036854775807
+    long l2 = 1_000_000;
 
     // Числа с плавающей точкой
     float f = 0.7f;             // от 4.9e-324 до 1.8e+308
@@ -19,4 +20,31 @@ public class PrimitiveDataTypes {
     // Логические
     boolean b1 = true;          // true или false
     boolean b2 = false;
+
+    public static void main(String[] args) {
+
+        // Особенности при вычислении примитивных типов с плавающей точкой связанные с IEEE-754
+        float f1 = 0.7f;
+        float f2 = 0.4f;
+
+        System.out.println(f1 - f2);
+
+        float f3 = 0.9f;
+        float f4 = 0.5f;
+
+        System.out.println(f3 - f4);
+
+        // Пример объявления примитивного типа char представляющего символы Unicode
+        char ch = 85;
+
+        System.out.println(ch);
+
+        char ch2 = 85 + 3;
+
+        System.out.println(ch2);
+
+        char ch3 = 'a' + 3;
+
+        System.out.println(ch3);
+    }
 }
